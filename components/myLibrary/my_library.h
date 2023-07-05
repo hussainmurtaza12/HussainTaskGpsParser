9eb0c3268c509eecdc677852cdc738748217c486
hussainmurtaza12 <137639513+hussainmurtaza12@users.noreply.github.com>
2023-07-06 03:03:14 +0500

    my_library.h

--- components/myLibrary/my_library.h
+++ components/myLibrary/my_library.h
@@ -6,16 +6,28 @@
 #include<string.h>
 // Define a struct for the parameters of the NMEA Sentence
 typedef struct{
-    double latitude;
-    double longitude;
-    double altitude;
-    double quality;
+    double latitude; //Latitude value
+    double longitude; // Longitude value
+    double altitude; // Altitude value
+    double quality; //Fix quality value
     int NoOfSatt; // Number of satellites
     double horPos; // Horizontal dilution of position
     double HoG; // Height of Geoid
     char checkSum[3]; // Checksum string
-    char time[11];
+    char time[11]; //Time string
 } GpsData;
+/**
+ * Calculate the checksum of an NMEA sentence.
+ * 
+ * @param sentence The NMEA sentence string.
+ * @return The calculated checksum.
+ */
 unsigned char nmea_checksum(const char *sentence);
+/**
+ * Parse the NMEA sentence and extract the GPS data.
+ * @param nmea_seq The NMEA sentence string.
+ * @param Gps_data Pointer to the GpsData struct to store the parsed data.
+ * @return 0 if parsing is successful, -1 if an error occurred.
+ */
 int parse_data(const char* nmea_seq, GpsData *Gps_data);
 #endif
